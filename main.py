0dddc1e66988b80adf3d5b29724ee495ff5839fc
diegosegura448 <38450723+diegosegura448@users.noreply.github.com>
2023-08-04 22:25:23 +0000

    clase 4

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 #Python
 #Ejercicio 1: 
+'''
 print('Hola Mundo.')
 
+#Ejercicio 2:
+Mensaje= 'Hola Mundo'
+print(Mensaje)
+
+#Ejercicio 3:
+nombre=input("Escribe tu nombre: ")
+print("Hola "+nombre+"!")
+ 
+#Ejercicio 4:
+print(((3+2)/(2*5))**2)
+
+#Ejercicio 5:
+horas=float(input("escriba la cantidad de horas: "))
+costo=float(input("escriba valor por hora: "))
+pago= horas*costo
+print("Tu pago es: ",pago)
+#Ejercicio 6
+n= int(input("ENtero positivi: "))
+suma= n*(n+1)/2
+print("La suma del entero es: ",suma) '''
+#ejercicio 7:
+peso=input("Digita tu peso en Kg: ")
+estatura= input("Digita tu estatura en M:")
+imc=round(float(peso)/float(estatura)**2,2)
+print("Tú indice de masa corporal es: ",imc)
