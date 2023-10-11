b5136b39be516661a3862dcc91b186b3b4023a8b
diegosegura448 <38450723+diegosegura448@users.noreply.github.com>
2023-10-11 03:27:45 +0000

    dicc

--- main.py
+++ main.py
@@ -725,5 +725,72 @@ def numeroCuadrado():
       print("%d -> %d" % (num,valor))
 
 numeroCuadrado()
-'''
 
+#repite cadena
+def cadenaRep():
+  dict = {}
+  cadena = input("Dame una cadena:")
+  for caracter in cadena:
+  	if caracter in dict:
+  		dict[caracter]+=1
+  	else:
+  		dict[caracter]=1	
+  
+  for campo,valor in dict.items():
+  	print (campo,"->",valor)
+
+cadenaRep()
+
+#peso frutas
+def precioF():
+  frutas = {'Plátano':1.35, 'Manzana':0.8, 'Pera':0.85, 'Naranja':0.7}
+  fruta = input('¿Qué fruta quieres? ').title()
+  kg = float(input('¿Cuántos kilos? '))
+  if fruta in frutas:
+      print(kg, 'kilos de', fruta, 'valen', frutas[fruta]*kg, '$')
+  else: 
+      print("Lo siento, la fruta", fruta, "no está disponible.")
+
+precioF()
+
+#cleintes diccionario
+def clienteDic():
+  clientes = {}
+  opcion = ''
+  while opcion != '6':
+      if opcion == '1':
+          cc = input('Introduce CC del cliente: ')
+          nombre = input('Introduce el nombre del cliente: ')
+          direccion = input('Introduce la dirección del cliente: ')
+          telefono = input('Introduce el teléfono del cliente: ')
+          email = input('Introduce el correo electrónico del cliente: ')
+          vip = input('¿Es un cliente preferente (S/N)? ')
+          cliente = {'nombre':nombre, 'dirección':direccion, 'teléfono':telefono, 'email':email, 'preferente':vip=='S'}
+          clientes[cc] = cliente
+      if opcion == '2':
+          nif = input('Introduce CC del cliente: ')
+          if nif in clientes:
+              del clientes[cc]
+          else:
+              print('No existe el cliente con el cc', nif)
+      if opcion == '3':
+          nif = input('Introduce CC del cliente: ')
+          if nif in clientes:
+              print('CC:', cc)
+              for clave, valor in clientes[cc].items():
+                  print(clave.title() + ':', valor)
+          else:
+              print('No existe el cliente con el cc', cc)
+      if opcion == '4':
+          print('Lista de clientes')
+          for clave, valor in clientes.items():
+              print(clave, valor['nombre'])
+      if opcion == '5':
+          print('Lista de clientes preferentes')
+          for clave, valor in clientes.items():
+              if valor['preferente']:
+                  print(clave, valor['nombre'])
+      opcion = input('Menú de opciones\n(1) Añadir cliente\n(2) Eliminar cliente\n(3) Mostrar cliente\n(4) Listar clientes\n(5) Listar clientes preferencial\n(6) Terminar\nElige una opción:')
+
+clienteDic()
+'''
