e3f208bd2fd5a7ba46dbf6ee7b006bf830914956
diegosegura448 <38450723+diegosegura448@users.noreply.github.com>
2023-10-11 23:56:26 +0000

    qwqw

--- main.py
+++ main.py
@@ -741,6 +741,17 @@ def cadenaRep():
 
 cadenaRep()
 
+#persona
+def nombresDic():
+  nombre = input('¿Cómo te llamas? ')
+  edad = input('¿Cuántos años tienes? ')
+  direccion = input('¿Cuál es tu dirección? ')
+  telefono = input('¿Cuál es tu número de teléfono? ')
+  persona = {'nombre': nombre, 'edad': edad, 'direccion': direccion, 'telefono': telefono}
+  print(persona['nombre'], 'tiene', persona['edad'], 'años, vive en', persona['direccion'], 'y su número de teléfono es', persona['telefono'])
+
+nombresDic()
+
 #peso frutas
 def precioF():
   frutas = {'Plátano':1.35, 'Manzana':0.8, 'Pera':0.85, 'Naranja':0.7}
@@ -794,3 +805,4 @@ def clienteDic():
 
 clienteDic()
 '''
+
